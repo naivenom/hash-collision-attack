58361eb2a4660fe952885238b749d57e7d3dc262
naivenom <naivenom@protonmail.com>
2017-12-06 18:42:28 +0100

    Update hash-collision-attack.py
    New two functions

--- hash-collision-attack.py
+++ hash-collision-attack.py
@@ -6,7 +6,7 @@
 ## [Details]:
 ## Script to perform a A Hash Collision Attack is an attempt to find two input strings of a hash function that produce the same hash result.
 ## Because hash functions have infinite input length and a predefined output length, there is inevitably going to be the possibility of two different inputs that produce the same output hash.
-## If two separate inputs produce the same hash output, it is called a collision. This collision can then be exploited by any application that compares two hashes together – such as password hashes, file integrity checks, etc.
+## If two separate inputs produce the same hash output, it is called a collision. This collision can then be exploited by any application that compares two hashes together â€“ such as password hashes, file integrity checks, etc.
 ##------------------------------------------------------------
 ## [Usage]:
 ## python3 hash-collision-attack.py --hashcode 0x21DD09EC --chunkbytes 5 --vulnapp col
@@ -44,6 +44,28 @@ vulnapp = results.vulnapp
 
 divide = int(hashcode, 16)/int(chunkbytes)
 
+def explotationFloat():
+	print('[+] EXPLOTATION:')
+	reverse_array1 = array1.reverse()
+	reverse_array2 = array2.reverse()
+	stri1 = ''.join(array1)
+	exploit1 = "'"+stri1+"'"
+	print('\t',exploit1)
+	stri2 = ''.join(array2)
+	exploit2 = "'"+stri2+"'"
+	print('\t',exploit2)
+	print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
+	os.system('./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
+
+def explotationInteger():
+	print('[+] EXPLOTATION:')
+	reverse_array1 = array1.reverse()
+	stri1 = ''.join(array1)
+	exploit1 = "'"+stri1+"'"
+	print('\t',exploit1)
+	print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
+	os.system('./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
+
 if type(divide) == float:
 	roundvalue = int(divide)
 	sub = int(hashcode, 16) - roundvalue*(int(chunkbytes)-1)
@@ -61,17 +83,7 @@ if type(divide) == float:
 		for i in range(int(len(hex2Withoutx))//2):
 			array2.append(str(x)+hex2WithoutxPlus0[c:2+c])
 			c = c+2
-		print('[+] EXPLOTATION:')
-		reverse_array1 = array1.reverse()
-		reverse_array2 = array2.reverse()
-		stri1 = ''.join(array1)
-		exploit1 = "'"+stri1+"'"
-		print('\t',exploit1)
-		stri2 = ''.join(array2)
-		exploit2 = "'"+stri2+"'"
-		print('\t',exploit2)
-		print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
-		os.system('./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
+		explotationFloat()
 	else:
 		hex1WithoutxPlus0 = "0"+hex1Withoutx
 		hex2WithoutxPlus0 = "0"+hex2Withoutx
@@ -82,17 +94,7 @@ if type(divide) == float:
 		for i in range(int(len(hex2WithoutxPlus0))//2):
 			array2.append(str(x)+hex2WithoutxPlus0[c:2+c])
 			c = c+2
-		print('[+] EXPLOTATION:')
-		reverse_array1 = array1.reverse()
-		reverse_array2 = array2.reverse()
-		stri1 = ''.join(array1)
-		exploit1 = "'"+stri1+"'"
-		print('\t',exploit1)
-		stri2 = ''.join(array2)
-		exploit2 = "'"+stri2+"'"
-		print('\t',exploit2)
-		print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
-		os.system('./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
+		explotationFloat()
 
 else: #integer and round value, not float
 	hex1 = hex(divide)
@@ -102,22 +104,10 @@ else: #integer and round value, not float
 		for i in range(int(len(hex1Withoutx))//2):
 			array1.append(str(x)+hex1WithoutxPlus0[c:2+c])
 			c = c+2
-		print('[+] EXPLOTATION:')
-		reverse_array1 = array1.reverse()
-		stri1 = ''.join(array1)
-		exploit1 = "'"+stri1+"'"
-		print('\t',exploit1)
-		print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
-		os.system('./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
+		explotationInteger()
 	else:
 		hex1WithoutxPlus0 = "0"+hex1Withoutx
 		for i in range(int(len(hex1WithoutxPlus0))//2):
 			array1.append(str(x)+hex1WithoutxPlus0[c:2+c])
 			c = c+2
-		print('[+] EXPLOTATION:')
-		reverse_array1 = array1.reverse()
-		stri1 = ''.join(array1)
-		exploit1 = "'"+stri1+"'"
-		print('\t',exploit1)
-		print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
-		os.system('./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
+		explotationFloat()
